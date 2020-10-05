1429440172109599fb216b1cf6198c53aaa6b4fc
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-10-04 20:56:24 -0600

    .json and script questions

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ const Employee = require("./assets/utils/employees/employee");
 const Engineer = require("./assets/utils/employees/Engineer");
 const Intern = require("./assets/utils/employees/intern");
 const Manager = require("./assets/utils/employees/Manager");
+const HTML = require("./assets/utils/generate.html");
+
+// Questions and Prompts
+async function prompt() {
+    let responseDone = "";
+    do {
+         try {
+              response = await inquirer.prompt([
+
+                   {
+                        type: "input",
+                        name: "name",
+                        message: "What is the employee's name?: "
+                   },
+                   {
+                        type: "input",
+                        name: "id",
+                        message: "Enter the employee's ID: "
+                   },
+                   {
+                        type: "input",
+                        name: "email",
+                        message: "Enter the employee's email address: "
+                   },
+                   {
+                        type: "list",
+                        name: "role",
+                        message: "What what is the employee's role:",
+                        choices: [
+                             "Engineer",
+                             "Intern",
+                             "Manager"
+                        ]
+                   }
+              ]);
+  }
