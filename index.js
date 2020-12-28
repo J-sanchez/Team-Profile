0c22c8b86a87826aec43d91a8b1b5f3b5d9e13e9
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-12-27 18:36:18 -0700

    Index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const jest = require("jest");
+//const jest = require("jest");
+
 //const Employee = require("./assets/utils/employees/employee");
 const Engineer = require("./assets/utils/employees/Engineer");
 const Intern = require("./assets/utils/employees/intern");
